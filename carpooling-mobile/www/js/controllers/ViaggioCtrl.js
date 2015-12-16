fa97e7ac50ce7801a724d28020522f51d77b3b5f
chin8 <matteo.chini@fbk.eu>
2015-12-16 17:21:56 +0100

    add path on trip detail

--- carpooling-mobile/www/js/controllers/ViaggioCtrl.js
+++ carpooling-mobile/www/js/controllers/ViaggioCtrl.js
@@ -1,6 +1,6 @@
 angular.module('carpooling.controllers.viaggio', [])
 
-.controller('ViaggioCtrl', function ($scope, $rootScope, $state, $stateParams, $filter, UserSrv, Utils, StorageSrv, PassengerSrv) {
+.controller('ViaggioCtrl', function ($scope, $rootScope, $state, $stateParams, MapSrv,Config, $filter, UserSrv, Utils, StorageSrv, PassengerSrv) {
     $scope.travelDateFormat = 'dd MMMM yyyy';
     $scope.travelTimeFormat = 'HH:mm';
     $scope.driverInfo = {};
@@ -11,8 +11,39 @@ angular.module('carpooling.controllers.viaggio', [])
     // -1 rejected, 0 requested, 1 accepted
     $scope.bookingState = null;
 
+    var addPath = function (selectedtrip) {
+                $scope.pathLine = MapSrv.getTripPolyline(selectedtrip.route);
+            $scope.markers ={
+                start: {lat:Number(selectedtrip.route.from.lat),lng:Number(selectedtrip.route.from.lon), icon: {
+                        iconUrl: 'img/ic_start.png',
+                        iconSize: [18, 25],
+                        iconAnchor: [9, 25 ]
+                    } },
+                stop: {lat:Number(selectedtrip.route.to.lat),lng:Number(selectedtrip.route.to.lon),icon: {
+                        iconUrl:'img/ic_flag.png',
+                        iconSize: [18,25],
+                        iconAnchor: [0, 25]
+                    } }
+            }
+
+            var boundsArray = [];
+            var boundstart = [$scope.markers.start.lat, $scope.markers.start.lng];
+            boundsArray.push(boundstart);
+            var boundstop = [$scope.markers.stop.lat, $scope.markers.stop.lng];
+            boundsArray.push(boundstop);
+
+            if (boundsArray.length > 0) {
+                var bounds = L.latLngBounds(boundsArray);
+                MapSrv.getMap('tripMap').then(function (map) {
+                    map.fitBounds(bounds);
+                });
+            }
+    }
     var refreshTrip = function (trip) {
         $scope.selectedTrip = trip;
+        if ($scope.selectedTrip){
+addPath($scope.selectedTrip);
+        }
         console.log($scope.selectedTrip);
 
         $scope.isMine = $scope.selectedTrip.userId === StorageSrv.getUserId();
@@ -104,4 +135,25 @@ angular.module('carpooling.controllers.viaggio', [])
             console.log('accepted');
         }
     };
+
+  $scope.initMap = function () {
+        MapSrv.initMap('tripMap').then(function () {
+            //add polyline
+
+        })
+    }
+
+
+
+
+     angular.extend($scope, {
+        center: {
+            lat: Config.getLat(),
+            lng: Config.getLon(),
+            zoom: Config.getZoom()
+        },
+        events: {},
+        markers:{},
+        pathLine:{}
+    });
 });
